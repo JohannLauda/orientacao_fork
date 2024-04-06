20b080bfd16d43422664feb250f7eef91ac34996
JohannLauda <jlauda3@yahoo.com.br>
2024-04-06 16:00:13 -0300

    alteracoes

--- Animal.py
+++ Animal.py
@@ -1,17 +1,19 @@
 class Animal:
-    def __init__(self, numero_de_patas: int) -> None:
+    def __init__(self, numero_de_patas: int):
         self.numero_de_patas = numero_de_patas
         
-    def fazer_barulho(self) -> None:
-        print("Eu sou um animal")
+    def fazer_barulho(self) -> str:
+        self.barulho = "Eu sou um animal"
+        return self.barulho
         
 class Cachorro(Animal):
-    def __init__(self, numero_de_patas: int, cor: str) -> None:
+    def __init__(self, numero_de_patas: int, cor: str) -> str:
         super().__init__(numero_de_patas)
-        self.__cor = cor
+        self.cor = cor
         
-    def fazer_barulho(self) -> None:
-        print("AU AU !!!!")
+    def fazer_barulho(self) -> str:
+        self.auau = "AU AU !!!!"
+        return self.auau
         
 
     
--- TesteAnimal.py
+++ TesteAnimal.py
@@ -7,6 +7,9 @@ print(capivara.fazer_barulho())
 
 doguinho = Cachorro(4, 'caramelo')
 
-print(doguinho.numero_de_patas, doguinho.cor)
+print(doguinho.numero_de_patas, "\n" + doguinho.cor)
 print(doguinho.fazer_barulho())
 
+dogao = Cachorro(4, 'preto')
+print(dogao.numero_de_patas, "\n" + dogao.cor)
+print(dogao.fazer_barulho())
